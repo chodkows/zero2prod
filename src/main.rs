8bb8327b56858cf40bab71edc4ef19c86b2f01cb
chodkows <chodkows@gmail.com>
2025-05-09 19:27:33 +0200

    Add configuration yaml and prepare connection string

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 use std::net::TcpListener;
 
-use zero2prod::run;
+use zero2prod::{
+    configuration::{self, get_configuration},
+    run,
+};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind a random port");
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
     run(listener)?.await
 }
